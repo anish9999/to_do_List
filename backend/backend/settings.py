be9affa4f400574aaa824ad748df37c88b03d1ba
anish9999 <anishshah9876@gmail.com>
2023-08-15 10:35:19 +0545

    changed Debug in setting

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-c%i9)4_wk2qq+-a%g%q)g1$$rm9c#e&z-8f0d0fh4ebg-#+^9z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
+DEBUG = True
 
 ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1']
 
